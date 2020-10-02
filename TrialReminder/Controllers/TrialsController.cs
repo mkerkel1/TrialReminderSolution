6d10df2a4d6a5619b2a2d8aa9ee5e042290a3996
mkerkel1 <papa.odin@gmail.com>
2020-10-02 10:57:05 -0400

    Morning Break.

---
+++ TrialReminder/Controllers/TrialsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TrialReminder.Models;
+
+namespace TrialReminder.Controllers
+{
+    public class TrialsController : Controller
+    {
+        private readonly TrialsDataContext _context;
+
+        public TrialsController(TrialsDataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> IndexAsync()
+        {
+            var results = await _context.Trials.ToListAsync();
+            return View(results);
+        }
+    }
+}
---
+++ TrialReminder/Models/Trials/TrialsSummaryModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrialReminder.Models.Trials
+{
+    public class TrialsSummaryModel
+    {
+        public List<TrialSummaryModelItem> Trials { get; set; }
+
+        public int NumberOfcurrentTrials { get; set; }
+        public int NumberOfExpiredTrials { get; set; }
+    }
+
+    public class TrialSummaryModelItem
+    {
+        public int Id { get; set; }
+        public string ServiceName { get; set; }
+        public string Url { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        public bool HasStartDate { get; set; }
+
+        public bool HasEndDate { get; set; }
+        public bool Expired { get; set; }
+        public int DaysLeft { get; set; }
+
+    }
+}
